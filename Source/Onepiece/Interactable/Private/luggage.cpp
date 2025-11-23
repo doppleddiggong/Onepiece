6fd93bbbd271977269a6d9d1ca090ebd0bc82cc6
dopple <ju100.bae@gmail.com>
2025-11-23 11:15:56 +0900

    fix: 불필요한 매 프레임 Tick 호출 비활성화
    기본적으로 매 프레임마다 Tick 함수를 호출하도록 설정되어 있었는데,
    이로 인해 성능에 불필요한 부담이 발생할 수 있어 변경을 진행했습니다.
    실제로 Tick 호출이 필요하지 않은 상황이라면 이를 비활성화하는 것이 효율적이기 때문입니다.
    
    - Aluggage 클래스에서 PrimaryActorTick.bCanEverTick 값을 false로 변경
    - 매 프레임 Tick 호출을 중지하여 성능 최적화
    - 불필요한 연산을 줄여 게임 실행 효율 개선

--- Source/Onepiece/Interactable/Private/luggage.cpp
+++ Source/Onepiece/Interactable/Private/luggage.cpp
@@ -9,8 +9,7 @@
 // Sets default values
 Aluggage::Aluggage()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 	SetRootComponent(Mesh);
