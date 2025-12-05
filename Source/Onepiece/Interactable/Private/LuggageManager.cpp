d6452c4a9670498454939389dbcada4cb961fdea
dopple <ju100.bae@gmail.com>
2025-12-05 18:07:26 +0900

    feat : 코드 정리

--- Source/Onepiece/Interactable/Private/LuggageManager.cpp
+++ Source/Onepiece/Interactable/Private/LuggageManager.cpp
@@ -13,21 +13,7 @@
 ALuggageManager::ALuggageManager()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-
-// Called when the game starts or when spawned
-void ALuggageManager::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ALuggageManager::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-	
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 void ALuggageManager::StartSpawning()
@@ -85,4 +71,3 @@ void ALuggageManager::SpawnLuggage()
 
 	CurrentSpawnIndex++;
 }
-
