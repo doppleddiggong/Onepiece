1161726c5d9a10ddc9779a3a3cad1e04310ce0ab
cubee021 <cubee021@users.noreply.github.com>
2025-12-30 06:07:47 +0000

    deploy: f535c400763e62217d29b7b4f8bcea109b77f5cd

--- doxygen/classAFoodHolder.js
+++ doxygen/classAFoodHolder.js
@@ -3,6 +3,7 @@ var classAFoodHolder =
     [ "AFoodHolder", "classAFoodHolder.html#ae9d05f11b8bd8e0caa568aa5e4980981", null ],
     [ "BeginPlay", "classAFoodHolder.html#a565e6ccdb08bb878bd88d8eb27dee677", null ],
     [ "CheckFood", "classAFoodHolder.html#a13aeb1948bac4f9ca7d91da7e3690625", null ],
+    [ "CheckPartialAnswer", "classAFoodHolder.html#aae82de2498ae535bc84c6e1a1598d841", null ],
     [ "GetLifetimeReplicatedProps", "classAFoodHolder.html#add78a30de6b665ebeb227151656bd4db", null ],
     [ "Multicast_ShowResultPopup", "classAFoodHolder.html#a555cc5bd6fbe893c6737eff179e263d6", null ],
     [ "Multicast_ShowWrongPopup", "classAFoodHolder.html#a61a099c315b83eb124af26ea9a44fcf6", null ],
